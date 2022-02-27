3bf59aef4f1f024bc6c04a6b153de778d4f36898
Kyungseo Lee <lackshata@gmail.com>
2022-02-27 13:16:39 +0900

    comiit

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float firstSpawn;
+    public float spawnInterval;
+
+    private void Start()
+    {
+        firstSpawn = 0;
+        spawnInterval = 1.5f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        firstSpawn += Time.deltaTime;
+        if (firstSpawn < spawnInterval) return;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            firstSpawn = 0;
+        }
+    }
+}
---
+++ challenge/Assets/ModTheCube/Cube.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    public float turnSpeed;
+    private Vector2 cubePosition;
+    
+    void Start()
+    {
+        InvokeRepeating("ChangePosition", 3.0f, 3.0f);
+        InvokeRepeating("ChangeColor", 2.0f, 2.5f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(turnSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void ChangePosition()
+    {
+        cubePosition = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+        transform.position = cubePosition;
+        transform.localScale = Vector3.one * 1.3f;
+    }
+
+    private void ChangeColor()
+    {
+        Material material = Renderer.material;
+        material.color = new Color(Random.Range(0.0f,1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+}
