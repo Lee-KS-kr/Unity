0ca2d52d65bd5cef8240e295027a1dd95de77227
Kyungseo Lee <lackshata@gmail.com>
2022-03-14 00:26:36 +0900

    crouch, jump

--- 3D FPS/Assets/Scripts/PlayerController.cs
+++ 3D FPS/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
     //스피드 조정 변수
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
+    [SerializeField] private float crouchSpeed;
     private float applySpeed;
 
+    [SerializeField] private float jumpForce;
+
     //상태 변수
+    private bool isCrouch = false;
     private bool isRun = false;
+    private bool isGround = true;
+
+    // 앉았을때 얼마나 앉을지 결정하는 변수
+    [SerializeField] private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
 
     //카메라 민감도
     [SerializeField] private float lookSensitivity;
@@ -22,21 +33,50 @@ public class PlayerController : MonoBehaviour
     //필요한 컴포넌트
     private Rigidbody playerRb;
     [SerializeField] private Camera theCamera;
+    private CapsuleCollider capsuleCollider;
+    #endregion
 
     private void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         playerRb = GetComponent<Rigidbody>();
+        originPosY = theCamera.transform.localPosition.y;
         applySpeed = walkSpeed;
+        applyCrouchPosY = originPosY;
     }
 
     private void Update()
     {
+        IsGround();
+        TryJump();
         TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
     }
 
+    #region Jump Method
+    private void IsGround()
+    {
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        playerRb.velocity = transform.up * jumpForce;
+    }
+    #endregion
+
+    #region Run Method
     private void TryRun()
     {
         if (Input.GetKey(KeyCode.LeftShift))
@@ -60,7 +100,47 @@ public class PlayerController : MonoBehaviour
         isRun = false;
         applySpeed = walkSpeed;
     }
+    #endregion
 
+    #region Crouch Method
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    private void Crouch()
+    {
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        }
+        else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    private IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        while (_posY != applyCrouchPosY)
+        {
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region Move and Rotaion Method
     private void Move()
     {
         float _moveDirX = Input.GetAxisRaw("Horizontal");
@@ -87,7 +167,8 @@ public class PlayerController : MonoBehaviour
     private void CharacterRotation()
     {
         float _yRotation = Input.GetAxisRaw("Mouse X");
-        Vector3 _characterRotationY = new Vector3(0f, -_yRotation, 0f) * lookSensitivity;
+        Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
         playerRb.MoveRotation(playerRb.rotation * Quaternion.Euler(_characterRotationY));
     }
+    #endregion
 }
